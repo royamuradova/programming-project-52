e3ed268c50f637b505a300e544fec76b75b3912f
royamuradova <royamuradova1998@gmail.com>
2024-12-02 03:18:28 +0000

    PATTERN CODE WORKS

--- main.cpp
+++ main.cpp
@@ -1,40 +1,27 @@
 #include <iostream>
-#include <fstream>
-#include <string>
 using namespace std;
 
 int main() {
-    string townName, fileName;
-
-    // Ask for town name and file name
-    cout << "Enter the name of the town: ";
-    getline(cin, townName);
-    cout << "Enter the name of the data file: ";
-    cin >> fileName;
-
-    // Open the file
-    ifstream inputFile(fileName);
-    if (!inputFile) {
-        cerr << "Error: Could not open file " << fileName << endl;
-        return 1;
-    }
-
-    // Display the header
-    cout << "\n" << townName << " Population Growth" << endl;
-    cout << "(each * represents 1,000 people)\n\n";
-
-    // Read data from file and display the bar chart
-    int year, population;
-    while (inputFile >> year >> population) {
-        cout << year << " ";
-        for (int i = 0; i < population / 1000; ++i) {
-            cout << "*";
+    // Pattern A
+    cout << "Pattern A\n";
+    for (int i = 1; i <= 10; ++i) {
+        for (int j = 1; j <= i; ++j) {
+            cout << "+";
         }
         cout << endl;
     }
 
-    // Close the file
-    inputFile.close();
+    // Spacer between patterns
+    cout << endl;
+
+    // Pattern B
+    cout << "Pattern B\n";
+    for (int i = 10; i >= 1; --i) {
+        for (int j = 1; j <= i; ++j) {
+            cout << "+";
+        }
+        cout << endl;
+    }
 
     return 0;
 }
